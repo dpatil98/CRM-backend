5818f2750c00487c52a48c93aa6c68628cb0883b
Dhananjay Patil <dsp5898@gmail.com>
2021-12-31 14:57:13 +0530

    Edit ,Del functionalities to Leads

--- routes/dashboardRoutes.js
+++ routes/dashboardRoutes.js
@@ -2,6 +2,7 @@
 import express from 'express';
 import{client} from "../index.js"
 import auth  from '../middleware/auth.js'
+import { ObjectId } from 'mongodb';
 
 const dashboardrouter = express.Router();
 
@@ -29,7 +30,6 @@ dashboardrouter.post("/AddLead", auth , async(request, response )=>{
     const{email} =request.body;
     const lead = request.body;
     
-
     const result = await CheckEmail(email);
     //  console.log("Add Lead",result);
     if(result)
@@ -45,6 +45,56 @@ dashboardrouter.post("/AddLead", auth , async(request, response )=>{
     
 });
 
+dashboardrouter.post("/EditLead", auth, async(request, response )=>{
+
+    const{assignedEmp ,email} =request.body;
+    const leadData = request.body;
+    
+    const  AE_firstName = assignedEmp.split(" ")[0];
+    const  AE_lastName = assignedEmp.split(" ")[1];
+
+    
+    const result = await CheckEmp(AE_firstName,AE_lastName);
+    // const Emailresult = await CheckEmail(email);
+    //  console.log("Add Lead",result);
+    if(!result)
+    {
+      response.status(401).json({message :"invalid Employee Name Or Repetative Email" , status:"401" });  
+    }
+    else{
+         await UpdateLead(leadData);
+        //  console.log("LeadData", leadData);
+         response.json({message :"User Register Successfully" , status:"200" });
+
+     }
+    
+});
+
+dashboardrouter.post("/DeleteLead", auth, async(request, response )=>{
+
+    const{id} =request.body;
+    const leadData = request.body; 
+    // const  AE_firstName = assignedEmp.split(" ")[0];
+    // const  AE_lastName = assignedEmp.split(" ")[1];
+
+    
+    // const result = await CheckEmp(AE_firstName,AE_lastName);
+    const result = await CheckCustomer(id);
+    // const Emailresult = await CheckEmail(email);
+    //  console.log("Delete result",id);
+    if(!result)
+    {
+      response.status(401).json({message :"Invalid Customer" , status:"401" });  
+    }
+    else{
+         await DeleteLead(id);
+         console.log("Deleted", leadData);
+         response.json({message :"Lead Deleted Successfully" , status:"200" });
+
+     }
+    
+});
+
 async function postLead(lead) {
     return await client.db("CRMUsers").collection("customers").insertOne(lead);
 }
@@ -52,5 +102,23 @@ async function postLead(lead) {
 async function CheckEmail(email) {
     return await client.db("CRMUsers").collection("customers").findOne({ email: email });
 }
+
+async function CheckEmp(FName, LName) {
+    return await client.db("CRMUsers").collection("users").findOne({  firstName:FName , lastName:LName });
+}
+
+async function UpdateLead(LeadData) {
+    return await client.db("CRMUsers").collection("customers").updateOne({_id : ObjectId(LeadData.id) }, { $set: { status:LeadData.status ,firstName:LeadData.firstName ,lastName:LeadData.lastName, email:LeadData.email ,assignedEmp:LeadData.assignedEmp , date:LeadData.date} });
+}
+
+async function CheckCustomer(id) {
+    return await client.db("CRMUsers").collection("customers").findOne( {_id : ObjectId(id) });
+
+}
+
+async function DeleteLead(id) {
+    return await client.db("CRMUsers").collection("customers").deleteOne({_id : ObjectId(id)});
+    }
+
 
 export const DashboardRouter = dashboardrouter;
